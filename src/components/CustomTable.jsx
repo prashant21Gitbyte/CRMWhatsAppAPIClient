d7c9515c7d9ce0b57dd4692cd3c3fa9f90af18b9
kuldeepcrmlanding <kuldeep.jangid@crmlanding.in>
2025-06-14 11:53:22 +0530

    update code

--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BsThreeDotsVertical } from "react-icons/bs"; // Add this
+import { BsThreeDotsVertical } from "react-icons/bs";
 import "../components/CSS/CustomTable.css";
  
 function CustomTable({ headers = [], columns = [], data = [] }) {
@@ -24,51 +24,51 @@ function CustomTable({ headers = [], columns = [], data = [] }) {
   const isAllSelected = data.length > 0 && selectedRows.length === data.length;
  
   return (
-   <div className="table-wrapper">
-  <div className="table-container">
-    <table>
-      <thead>
-        <tr>
-          <th>
-            <input
-              type="checkbox"
-              checked={isAllSelected}
-              onChange={handleSelectAll}
-            />
-          </th>
-          {headers.map((header, idx) => (
-            <th key={idx}>{header}</th>
-          ))}
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <td>
-              <input
-                type="checkbox"
-                checked={selectedRows.includes(rowIndex)}
-                onChange={() => handleSelectRow(rowIndex)}
-              />
-            </td>
-            {columns.map((col, colIndex) => (
-              <td key={colIndex}>{row[col]}</td>
+    <div className="custom-table-wrapper">
+      <div className="custom-table-container">
+        <table className="custom-table">
+          <thead className="custom-thead">
+            <tr>
+              <th>
+                <input
+                  type="checkbox"
+                  checked={isAllSelected}
+                  onChange={handleSelectAll}
+                />
+              </th>
+              {headers.map((header, idx) => (
+                <th key={idx}>{header}</th>
+              ))}
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody className="custom-tbody">
+            {data.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedRows.includes(rowIndex)}
+                    onChange={() => handleSelectRow(rowIndex)}
+                  />
+                </td>
+                {columns.map((col, colIndex) => (
+                  <td key={colIndex}>{row[col]}</td>
+                ))}
+                <td>
+                  <button className="custom-kebab-menu">
+                    <BsThreeDotsVertical />
+                  </button>
+                </td>
+              </tr>
             ))}
-            <td>
-              <button className="kebab-menu">
-                <BsThreeDotsVertical />
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-</div>
- 
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
  
 export default CustomTable;
+ 
  
